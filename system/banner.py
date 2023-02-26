2e047b3f5c57d119c714960305775ae57d116883
A l f i n d o <75003444+alfinkresna@users.noreply.github.com>
2023-02-26 17:50:43 +0700

    Add files via upload

---
+++ system/banner.py
@@ -0,0 +1,22 @@
+banner = """
+    Berita Terbaru
+    
+[1] Kemenkumham
+
+[2] Antaranews (Hukum)
+
+[3] Antaranews (Politik)
+
+[4] Antaranews (Kriminalitas)
+
+[5] Vice Indonesia
+
+[6] CNN Indonesia
+
+[7] CNBC Indonesia
+
+[8] Cyware (Cybersecurity)
+
+[9] F5 (Threats)
+
+[0] Keluar"""
